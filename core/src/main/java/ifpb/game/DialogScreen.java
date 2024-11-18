368b865c441fe8c963f685aeb3cda6f6efae3419
Micaias <micaias.oliveira@academico.ifpb.edu.br>
2024-11-17 21:12:40 -0300

    logica inicial adicionada

--- core/src/main/java/ifpb/game/DialogScreen.java
+++ core/src/main/java/ifpb/game/DialogScreen.java
@@ -1,24 +1,58 @@
 package ifpb.game;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /**
  * First screen of the application. Displayed after the application is created.
  */
-public class FirstScreen implements Screen {
+public class DialogScreen implements Screen {
+
+    FitViewport viewport;
+    SpriteBatch spriteBatch;
+
     @Override
     public void show() {
         // Prepare your screen here.
+
+        viewport = new FitViewport(160, 90);
+        spriteBatch = new SpriteBatch();
+
     }
 
     @Override
     public void render(float delta) {
         // Draw your screen here. "delta" is the time since last render in seconds.
+        input();
+        logic(delta);
+        draw();
+
+    }
+
+    private void input() {
+
+    }
+
+    private void logic(float delta) {
+        System.out.println(delta);
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        spriteBatch.end();
     }
 
     @Override
     public void resize(int width, int height) {
         // Resize your screen here. The parameters represent the new window size.
+        viewport.update(width, height, true); // true centers the camera
     }
 
     @Override
